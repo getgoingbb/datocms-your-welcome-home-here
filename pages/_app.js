d355031b2c091e3efb54ff720d9e0d835229fcbf
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 17:27:16 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
-import '../styles/index.css'
+import '../styles/globals.css'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { GA_TRACKING_ID, pageview } from '../lib/analytics' // Adjust the import path as necessary
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
+

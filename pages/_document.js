70fe04aa9c9f3ca90e34cd71675466c3df9d35cf
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 17:42:47 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,20 @@
-import Document, { Html, Main, Head, NextScript } from "next/document";
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <title>My Next.js App</title>
-          <link rel="stylesheet" href="/styles/globals.css" />
-         {/* Example of adding a new script */}
-          <script src="lib/analytics.js" defer></script>
-        </Head>
+          {/* Existing tags */}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument

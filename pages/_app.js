4f05276553cbd8fac9c480dc5fb0a0816a9b22bc
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 16:46:09 -0500

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,69 +1,7 @@
 import '../styles/index.css'
-import Head from 'next/head';
-import { GA_TRACKING_ID } from '../path/to/analytics.js';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { pageview } from '../path/to/analytics.js';
-
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  // ... rest of your component
-}
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  // ... rest of your component
-}
-
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
-
-export default MyApp;
+export default MyApp

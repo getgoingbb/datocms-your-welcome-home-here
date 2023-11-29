cbd64207b1c63ba8d2311bf1d2f9ff1da386dff9
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 16:48:40 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
 import '../styles/index.css'
+import Head from 'next/head';
+import { GA_TRACKING_ID } from '../path/to/analytics.js';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;

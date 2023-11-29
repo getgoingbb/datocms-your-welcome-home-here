3d584393aa05f1caeb4bba518c781b0f140876a3
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 16:51:24 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Head from 'next/head';
+import { GA_TRACKING_ID } from '../path/to/analytics.js';
 
 export default class MyDocument extends Document {
   render() {
@@ -14,3 +16,32 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;

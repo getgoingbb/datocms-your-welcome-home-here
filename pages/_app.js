5297bc55565c54c4b0a456f2436f8530f5e64159
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 16:44:34 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,38 @@
 import '../styles/index.css'
 import Head from 'next/head';
 import { GA_TRACKING_ID } from '../path/to/analytics.js';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { pageview } from '../path/to/analytics.js';
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  // ... rest of your component
+}
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  // ... rest of your component
+}
 
 
 function MyApp({ Component, pageProps }) {

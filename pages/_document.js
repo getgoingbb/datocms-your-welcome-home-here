ff21f5e146bcb09a6662b1001eac83b3c03c7202
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-29 17:41:16 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <title>My Next.js App</title>
           <link rel="stylesheet" href="/styles/globals.css" />
          {/* Example of adding a new script */}
-          <script src="https://example.com/external-script.js" defer></script>
+          <script src="lib/analytics.js" defer></script>
         </Head>
         </Head>
         <body>

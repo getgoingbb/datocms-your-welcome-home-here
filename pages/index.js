3e63b359182abdc48b2e4205dcb819e540b8f1c9
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-30 18:16:40 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,16 @@ export default function Index({ subscription }) {
     <>
       <Layout preview={subscription.preview}>
         <Head>{renderMetaTags(metaTags)}</Head>
-        <Script id="google-analytics">
-          {`
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'G-R1TN5P38SJ', 'auto');
-            ga('send', 'pageview');
-          `}
-        </Script>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-R1TN5P38SJ" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
         <Container>
           <LanguageBar />
           <Intro />

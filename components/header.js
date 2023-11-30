8e3e92227c7a82de0800a346b3c8856bd6182614
BB <113383668+getgoingbb@users.noreply.github.com>
2023-11-30 18:31:50 -0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,14 +2,6 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R1TN5P38SJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){window.dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'GA_TRACKING_ID');
-</script>  
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
         <a className="hover:underline">Welcome Home</a>

a682a415371994ca0bb688173f2e43eb90ad2177
BB <113383668+getgoingbb@users.noreply.github.com>
2023-12-01 16:34:14 -0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -13,13 +13,13 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://welcomehomereferrals.info"
+              href="https://welcomehomereferrals.com/sellers"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               {i18n.footer.read[locale]}
             </a>
             <a
-              href="https://welcomehomereferrals.com"
+              href="https://welcomehomereferrals.info"
               className="mx-3 font-bold hover:underline"
             >
               {i18n.footer.github[locale]}

16a08f861c7dfcb295ee0c4e997425d95dffaac5
BB <113383668+getgoingbb@users.noreply.github.com>
2023-12-01 13:04:01 -0500

    Update alert.js

--- components/alert.js
+++ components/alert.js
@@ -7,35 +7,8 @@ export default function Alert({ preview }) {
   const { locale } = useRouter();
 
   return (
-    <div
-      className={cn("border-b", {
-        "bg-accent-7 border-accent-7 text-white": preview,
-        "bg-accent-1 border-accent-2": !preview,
-      })}
-    >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              {i18n.alert.draftContent[locale]}{" "}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                {i18n.alert.clickCta[locale]}
-              </a>{" "}
-              {i18n.alert.exitPreview[locale]}
-            </>
-          ) : (
-            <>
-              {i18n.alert.publishedContent[locale]}{" "}
-              <a
-                href="/api/preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                {i18n.alert.clickCta[locale]}
-              </a>{" "}
-              {i18n.alert.enterPreview[locale]}
+<div>
+    <h2>Welcome<h2>
             </>
           )}
         </div>

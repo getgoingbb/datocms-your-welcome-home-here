dc1a2a37aeb5ff7885265d629ae07bd16f10d0fe
BB <113383668+getgoingbb@users.noreply.github.com>
2023-12-22 13:38:57 -0500

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between items-center mt-16 mb-16 md:mb-12">
       <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-6">
-        Welcome Home Real Estate Referrals & Current Market
+        Welcome Home Real Estate Referrals & Current Market Conditions
       </h1>
       <div className="md:w-1/2 lg:w-1/3 w-full mt-10">
         <Search />
